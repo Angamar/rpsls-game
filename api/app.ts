44a474f0cc650284624f1a89a35fdfdedec2ce38
Aleksa Stojanovic <aleksa.stojanovic32@aspendental.com>
2025-06-04 23:35:28 +0200

    chore: convert files to TS - part 2

---
+++ api/app.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+import gameRoutes from './routes/gameRoutes';
+
+const app = express();
+const port = 3000;
+
+
+app.use(express.json());
+app.use("/api", gameRoutes);
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
---
+++ api/controllers/gameController.ts
@@ -0,0 +1,33 @@
+import { Request, RequestHandler, Response } from 'express';
+import { ChoiceListSchema } from '../schemas/choice.schema';
+// import axios from 'axios';
+
+
+const choices = [
+    { id: 1, name: 'Rock' },
+    { id: 2, name: 'Paper' },
+    { id: 3, name: 'Scissors' },
+    { id: 4, name: 'Lizard' },
+    { id: 5, name: 'Spock' },
+];
+
+export const getChoices = (req: Request, res: Response) => {
+    const parsed = ChoiceListSchema.safeParse(choices);
+    if (!parsed.success) {
+        res.status(500).json({ error: 'Invalid data format' });
+    }
+
+    res.json(parsed.data);
+};
+
+
+
+
+// export const getRandomChoice = async (req: Request, res: Response) => {
+//   try {
+//     const response = await axios.get('https://codechallenge.boohma.com/random');
+//     res.json(response.data);
+//   } catch (error: any) {
+//     res.status(500).json({ error: error.message });
+//   }
+// };
