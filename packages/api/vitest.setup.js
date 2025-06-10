2632479d62dd0b874defcd43e751010acd7e2e2b
Aleksa Stojanovic <aleksa_95@msn.com>
2025-06-10 22:41:04 +0200

    build: output shared package to dist

---
+++ packages/api/vitest.setup.js
@@ -0,0 +1,8 @@
+import { beforeAll, afterAll } from "vitest";
+// Example: Mock global variables or setup database connections
+beforeAll(() => {
+  console.log("Setting up tests...");
+});
+afterAll(() => {
+  console.log("Cleaning up after tests...");
+});
